d58e0f41a4cf439968fd27506e096e25362469a8
prasad <janagma.prasad@wipro.com>
2024-04-21 09:23:16 +0000

    adding

---
+++ src/main/java/com/wipro/stngutls.java
@@ -0,0 +1,19 @@
+package com.wipro;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class stngutls {
+
+	public static void main(String[] args) {
+		String str1 = "Hello";
+        String str2 = null;
+        String str3 = "";
+        String str4 = "  Welcome to the world of Java!  ";
+        
+        // Checking if a string is empty or null
+        System.out.println("Is str1 empty or null? " + StringUtils.isEmpty(str1));
+		
+
+	}
+
+}
